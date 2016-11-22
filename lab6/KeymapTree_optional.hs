c4e51cc1c05218866f89bb2199c3d48742d8d77d
Stefan Cobeli <stefancobeli@gmail.com>
2016-11-22 18:56:40 +0200

    Lab6 mandatory exercises solved. Upload optiional material unsolved.

--- lab6/KeymapTree_optional.hs
+++ lab6/KeymapTree_optional.hs
@@ -8,8 +8,7 @@ module KeymapTree ( Keymap,
                     get, set, del,
                     select,
                     toList, fromList,
-                    merge, filterLT, filterGT,
-					testTree, prop_set_get, prop_toList_fromList, prop_toList_fromList_sorted                  
+                    merge, filterLT, filterGT                  
                   )
 
 where
@@ -26,11 +25,6 @@ data Keymap k a = Leaf
                 | Node k a (Keymap k a) (Keymap k a)
 
 -- A test tree
-del = undefined 
-select = undefined
-merge = undefined
-filterLT = undefined
-filterGT = undefined
 
 testTree :: Keymap Int Int
 testTree = Node 2 20 (Node 1 10 Leaf Leaf)
@@ -44,14 +38,12 @@ size Leaf = 0
 size (Node _ _ left right) = 1 + size left + size right
 
 depth :: Ord k => Keymap k a -> Int
-depth Leaf = 0
-depth (Node _ _ lt rt) = 1 + max (depth lt) (depth rt)
+depth = undefined
 
 -- Exercise 7
 
 toList :: Ord k => Keymap k a -> [(k,a)]
-toList Leaf = []
-toList (Node k v lt rt) =  toList lt ++ [(k,v)] ++ toList rt
+toList = undefined
 
 -- Exercise 8
 
@@ -60,28 +52,21 @@ set key value = f
     where
       f Leaf = Node key value Leaf Leaf
       f (Node k v left right) | key == k  = Node k value left right
-                              | key <= k  = Node k v (f left) right 
-                              | otherwise = Node k v left (f right)
+                              | key <= k  = undefined
+                              | otherwise = undefined
 
 -- Exercise 9
 
 get :: Ord k => k -> Keymap k a -> Maybe a
-get kk Leaf                        = Nothing 
-get kk (Node key value left right) | kk == key = Just value
-								   | kk <  key = get kk left
-								   | otherwise = get kk right
+get = undefined
 
 prop_set_get :: Int -> Int -> Bool
 prop_set_get k v = get k (set k v testTree) == Just v
 
-
-
 -- Exercise 10
 
 fromList :: Ord k => [(k,a)] -> Keymap k a
---fromList = foldr (uncurry set) Leaf
-fromList [] = Leaf
-fromList ((k,v):xs) = set k v (fromList xs)
+fromList = undefined
 
 
 prop_toList_fromList :: [Int] -> [Int] -> Bool
@@ -93,6 +78,31 @@ prop_toList_fromList_sorted :: [Int] -> [Int] -> Bool
 prop_toList_fromList_sorted xs ys = toList (fromList zs) == sort zs
     where
       zs = zip (nub xs) ys
+
+-- Optional Material -----------------------------------
+
+-- Exercise 12
+
+filterLT :: Ord k => k -> Keymap k a -> Keymap k a
+filterLT = undefined
+
+filterGT :: Ord k => k -> Keymap k a -> Keymap k a
+filterGT = undefined
+
+-- Exercise 13
+
+merge :: Ord k => Keymap k a -> Keymap k a -> Keymap k a
+merge = undefined
+
+-- Exercise 14
+
+del :: Ord k => k -> Keymap k a -> Keymap k a
+del = undefined
+
+-- Exercise 15
+
+select :: Ord k => (a -> Bool) -> Keymap k a -> Keymap k a
+select = undefined 
 
 -- Instances for QuickCheck -----------------------------
 instance (Ord k, Show k, Show a) => Show (Keymap k a) where
--- lab6/lab6.hs
+++ lab6/lab6.hs
@@ -8,9 +8,9 @@ import System.Random
 
 -- Importing the keymap module
 
-import KeymapList
+import KeymapTree
 
-
+import Test.QuickCheck
 -- Type declarations
 
 type Barcode = String
@@ -43,22 +43,32 @@ formatLine :: Int -> (Barcode, Item) -> String
 formatLine l (code, (name, typ))= code ++ "..." ++ name ++ (3 + l - length name) `replicate` '.' ++ typ
 
 showCatalogue :: Catalogue -> String
-showCatalogue = undefined
+showCatalogue c = unlines (map 
+		(formatLine (longestProductLen (toList c)))
+		(toList c)) 
      
 -- Exercise 2
 maybeToList :: Maybe a -> [a]
-maybeToList = undefined
+maybeToList Nothing = []
+maybeToList (Just x)  = [x]
 
 listToMaybe :: [a] -> Maybe a
-listToMaybe = undefined
+listToMaybe [] = Nothing
+listToMaybe x  = Just (head x)
 
 catMaybes :: [Maybe a] -> [a]
-catMaybes = undefined
+catMaybes items = [x | (Just x) <- items]
+
+catMaybes2 :: [Maybe a] -> [a]
+catMaybes2 = concat . map maybeToList
+
+prop_cat :: Eq a => [Maybe a] -> Bool
+prop_cat list = catMaybes list == catMaybes2 list
 
 -- Exercise 3
 
 getItems :: [Barcode] -> Catalogue -> [Item]
-getItems = undefined
+getItems list c = catMaybes (map (flip get c) list) 
 
 
 
